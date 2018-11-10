2594284bcacce1386453f43270f47de49fb47571
Derstine Buyagan <bastedboyderzthine@gmail.com>
2018-11-10 13:39:17 +0800

    xf

--- WebApplication1/Controller/Hello.cs
+++ WebApplication1/Controller/Hello.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-namespace WebApplication1.Controller
+﻿namespace WebApi.Controller
 {
     public class Hello
     {
