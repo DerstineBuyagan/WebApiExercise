9c6943503988b44488d51c0005ab239769327fb2
Derstine Buyagan <bastedboyderzthine@gmail.com>
2018-11-10 13:15:48 +0800

    Add project files.

---
+++ WebApplication1/Controller/Hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Controller
+{
+    public class Hello
+    {
+        
+    }
+
+   
+}
